9994e928bac2f482309e39d8e572575a46bdb713
NIMA FATHIMA <nimafathimapr@gmail.com>
2024-08-25 20:19:37 +0530

    Update CODE.c

--- DS/LAB/stack/code.c
+++ DS/LAB/stack/code.c
@@ -4,48 +4,66 @@ int stack[100];
 int top = -1;
 int size = 5;
 
-void push(int x) {
-    if (top >= size - 1) {
+void push(int x)
+{
+    if (top >= size - 1) 
+    {
         printf("stack overflow\n");
-    } else {
+    }
+    else
+    {
         top++;
         stack[top] = x;
     }
 }
 
-int pop() {
-    if (top < 0) {
+int pop()
+{
+    if (top < 0)
+    {
         printf("stack underflow\n");
         return -1; // Return a sentinel value (e.g., -1) to indicate an error
-    } else {
+    } 
+    else
+    {
         int x = stack[top];
         top--;
         return x;
     }
 }
 
-int peek() {
-    if (top < 0) {
+int peek()
+{
+    if (top < 0)
+    {
         printf("stack underflow\n");
         return -1; // Return a sentinel value (e.g., -1) to indicate an error
-    } else {
+    } 
+    else
+    {
         return stack[top];
     }
 }
 
-void display() {
-    if (top < 0) {
+void display() 
+{
+    if (top < 0) 
+    {
         printf("stack is empty\n");
-    } else {
+    } 
+    else 
+    {
         printf("stack elements are:\n");
-        for (int i = 0; i <= top; i++) {
+        for (int i = 0; i <= top; i++)
+        {
             printf("%d ", stack[i]);
         }
         printf("\n");
     }
 }
 
-int main() {
+int main() 
+{
     int choice, value;
     while (1) {
         printf("\nmenu:\n");
@@ -57,7 +75,8 @@ int main() {
         printf("enter your choice: ");
         scanf("%d", &choice);
 
-        switch (choice) {
+        switch (choice)
+        {
             case 1:
                 printf("enter value to push: ");
                 scanf("%d", &value);
@@ -65,13 +84,15 @@ int main() {
                 break;
             case 2:
                 value = pop();
-                if (value != -1) {
+                if (value != -1)
+                {
                     printf("popped value is %d\n", value);
                 }
                 break;
             case 3:
                 value = peek();
-                if (value != -1) {
+                if (value != -1) 
+                {
                     printf("top element is %d\n", value);
                 }
                 break;
