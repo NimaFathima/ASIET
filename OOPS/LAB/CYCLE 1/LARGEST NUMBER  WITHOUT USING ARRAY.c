d21e4d0d0ffdbba4c5b922add7f3772d0ed98c36
NIMA FATHIMA <nimafathimapr@gmail.com>
2024-09-02 15:32:16 +0530

    Update LARGEST NUMBER  WITHOUT USING ARRAY.c

--- OOPS/LAB/CYCLE 1/LARGEST NUMBER  WITHOUT USING ARRAY.c
+++ OOPS/LAB/CYCLE 1/LARGEST NUMBER  WITHOUT USING ARRAY.c
@@ -1,28 +1,31 @@
 //Program to find THE LARGEST ELEMENT  - Nima Fathima46
-import java.util.*;
-class Largest
- {
-    	public static void main(String args[])
-         {
-        	 Scanner s = new Scanner(System.in);
-          int n,largest=0;
-        	 System.out.println("ENTER THE NUMBER OF ELEMENTS: ");
-        	 n = s.nextInt();
-        	 System.out.println("ENTER THE ELEMENTS:");
-        	 for (int i = 0; i < n; i++) 
-        	 {
-                int a = s.nextInt();
-                if(a>largest)
-                {
-                    largest=a;
-                }
-        	 }
-        	 System.out.println("THE LARGEST ELEMENT IS:"+largest);
-       }
+import java.util.Scanner;
+
+class Palindrome
+{
+    public static void main(String[] args) 
+    {
+        String S = "", rev = "";
+        Scanner s = new Scanner(System.in);
+        System.out.println("ENTER THE STRING: ");
+        S = s.nextLine();
+        S=S.toLowerCase();
+        int len = S.length();
+        
+        for (int i = len - 1; i >= 0; i--)
+        {
+            rev = rev + S.charAt(i);
+        }
+        
+        if (rev.equals(S))
+        {
+            System.out.println("THE GIVEN STRING IS A PALINDROME.");
+        } 
+        else 
+        {
+            System.out.println("THE GIVEN STRING IS NOT A PALINDROME.");
+        }
+    }
 }
-
-
-
-
 
 
