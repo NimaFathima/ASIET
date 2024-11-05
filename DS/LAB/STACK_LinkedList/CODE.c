b6ea9dbf7d18ee91db5ee240e2d41e5878236b47
NIMA FATHIMA <nimafathimapr@gmail.com>
2024-11-05 09:50:02 +0530

    Update CODE.c

--- DS/LAB/STACK_LinkedList/CODE.c
+++ DS/LAB/STACK_LinkedList/CODE.c
@@ -1,70 +1,76 @@
-#include <limits.h>
 #include <stdio.h>
 #include <stdlib.h>
 
-typedef struct Node {
+struct Node {
     int data;
     struct Node* next;
-} Node;
+};
 
-Node* createNode(int new_data) {
-    Node* new_node = (Node*)malloc(sizeof(Node));
-    new_node->data = new_data;
-    new_node->next = NULL;
-    return new_node;
-}
-
-typedef struct Stack {
-    Node* head;
-} Stack;
-
-void initializeStack(Stack* stack) { stack->head = NULL; }
-
-int isEmpty(Stack* stack) {
-    return stack->head == NULL;
-}
-
-void push(Stack* stack, int new_data) {
-    Node* new_node = createNode(new_data);
+void push(struct Node** top, int value) {
+    struct Node* new_node = (struct Node*)malloc(sizeof(struct Node));
     if (!new_node) {
-        printf("\nStack Overflow");
+        printf("Memory allocation failed\n");
         return;
     }
-    new_node->next = stack->head;
-    stack->head = new_node;
+    new_node->data = value;
+    new_node->next = *top;
+    *top = new_node;
 }
 
-void pop(Stack* stack) {
-    if (isEmpty(stack)) {
-        printf("\nStack Underflow\n");
+int pop(struct Node** top) {
+    if (*top == NULL) {
+        printf("Stack Underflow\n");
+        return -1;
+    }
+    struct Node* temp = *top;
+    int popped_value = temp->data;
+    *top = (*top)->next;
+    free(temp);
+    return popped_value;
+}
+
+int peek(struct Node* top) {
+    if (top == NULL) {
+        printf("Stack is empty\n");
+        return -1;
+    }
+    return top->data;
+}
+
+int isEmpty(struct Node* top) {
+    return top == NULL;
+}
+
+void display(struct Node* top) {
+    if (top == NULL) {
+        printf("Stack is empty\n");
         return;
-    } else {
-        Node* temp = stack->head;
-        stack->head = stack->head->next;
-        free(temp);
     }
-}
-
-int peek(Stack* stack) {
-    if (!isEmpty(stack))
-        return stack->head->data;
-    else {
-        printf("\nStack is empty");
-        return INT_MIN;
+    struct Node* temp = top;
+    printf("Stack elements are: ");
+    while (temp != NULL) {
+        printf("%d ", temp->data);
+        temp = temp->next;
     }
+    printf("\n");
 }
 
 int main() {
-    Stack stack;
-    initializeStack(&stack);
-    push(&stack, 11);
-    push(&stack, 22);
-    push(&stack, 33);
-    push(&stack, 44);
-    printf("Top element is %d\n", peek(&stack));
-    printf("Removing two elements...\n");
-    pop(&stack);
-    pop(&stack);
-    printf("Top element is %d\n", peek(&stack));
+    struct Node* stack = NULL;
+
+    push(&stack, 10);
+    push(&stack, 20);
+    push(&stack, 30);
+
+    printf("Top element is: %d\n", peek(stack));
+
+    display(stack);
+
+    printf("Popped element is: %d\n", pop(&stack));
+
+    display(stack);
+
+    printf("Stack is empty: %d\n", isEmpty(stack));
+
     return 0;
 }
