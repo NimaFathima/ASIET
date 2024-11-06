5a02c9902a628429e8e8088b26da4a720a8d330d
NIMA FATHIMA <nimafathimapr@gmail.com>
2024-11-06 10:06:51 +0530

    Create check_if_prime.java

---
+++ OOPS/TUTORIAL/CYCLE 2/check_if_prime.java
@@ -0,0 +1,41 @@
+import java.util.Scanner;
+
+public class PrimeNumberCheck {
+
+    public static boolean isPrime(int num) {
+        if (num <= 1) {
+            return false; // 0 and 1 are not prime
+        }
+        for (int i = 2; i <= Math.sqrt(num); i++) {
+            if (num % i == 0) {
+                return false; // Divisible by i, not prime
+            }
+        }
+        return true; // Prime number
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int num = scanner.nextInt();
+
+        if (isPrime(num)) {
+            System.out.println(num + " is a prime number.");
+        } else {
+            System.out.println(num + " is not a prime number.");
+        }
+
+        scanner.close();
+    }
+}
+
+/*
+Example Output:
+
+Enter a number: 7
+7 is a prime number.
+
+Enter a number: 10
+10 is not a prime number.
+*/
