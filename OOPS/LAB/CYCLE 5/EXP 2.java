ed45623e1c0e84f6d3bdf7f6ca2c5fd8a7387f75
NIMA FATHIMA <nimafathimapr@gmail.com>
2024-11-06 09:08:26 +0530

    Update EXP 2.java

--- OOPS/LAB/CYCLE 5/EXP 2.java
+++ OOPS/LAB/CYCLE 5/EXP 2.java
@@ -1,12 +1,10 @@
 class Counter {
     private int count = 0;
 
-    // Synchronized method to increment the counter
     public synchronized void increment() {
         count++;
     }
 
-    // Method to get the current value of the counter
     public int getCount() {
         return count;
     }
@@ -30,23 +28,22 @@ public class ThreadSynchronizationDemo {
     public static void main(String[] args) {
         Counter counter = new Counter();
 
-        // Creating two threads that share the same counter object
         CounterThread thread1 = new CounterThread(counter);
         CounterThread thread2 = new CounterThread(counter);
 
-        // Starting both threads
         thread1.start();
         thread2.start();
 
         try {
-            // Waiting for both threads to finish
             thread1.join();
             thread2.join();
         } catch (InterruptedException e) {
             System.out.println("Thread interrupted: " + e.getMessage());
         }
 
-        // Printing the final value of the counter
         System.out.println("Final Counter Value: " + counter.getCount());
     }
 }
+
+//OUTPUT:
+//Final Counter Value: 2000
