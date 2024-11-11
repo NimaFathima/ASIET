d43515e0fcfd41fd3ddd5a5e2292f7ecec7ca24c
NIMA FATHIMA <nimafathimapr@gmail.com>
2024-11-11 09:32:19 +0530

    Update dfs new similar to bfs.java

--- DS/dfs new similar to bfs.java
+++ DS/dfs new similar to bfs.java
@@ -22,24 +22,27 @@ void addEdge(int adj[MAX][MAX], int u, int v) {
 
 int main() {
     // Number of vertices in the graph
-    int V = 5;
+    int V = 6;
 
     // Adjacency matrix representation of the graph
     int adj[MAX][MAX] = {0};
 
     // Add edges to the graph
-    addEdge(adj, 0, 1);
-    addEdge(adj, 0, 2);
-    addEdge(adj, 1, 3);
-    addEdge(adj, 1, 4);
-    addEdge(adj, 2, 4);
+    addEdge(adj, 1, 2);
+    addEdge(adj, 2, 0);
+    addEdge(adj, 0, 3);
+    addEdge(adj, 4, 5);
 
     // Initialize visited array
     int visited[MAX] = {0};
 
-    // Perform DFS traversal starting from vertex 0
-    printf("DFS starting from 0:\n");
-    dfs(adj, V, 0, visited);
+    // Perform DFS traversal from all vertices to ensure all nodes are covered
+    printf("DFS traversal of the graph:\n");
+    for (int i = 0; i < V; i++) {
+        if (!visited[i]) {
+            dfs(adj, V, i, visited);
+        }
+    }
 
     return 0;
 }
