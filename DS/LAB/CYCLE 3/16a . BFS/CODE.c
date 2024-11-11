cca170eeb0b15f574b0088c04f651d074b63dcbd
NIMA FATHIMA <nimafathimapr@gmail.com>
2024-11-11 07:49:17 +0530

    Update CODE.c

--- DS/LAB/CYCLE 3/16a . BFS/CODE.c
+++ DS/LAB/CYCLE 3/16a . BFS/CODE.c
@@ -1,44 +1,37 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <stdbool.h>
-
 
 #define MAX 100
 
-
 // BFS from given source s
 void bfs(int adj[MAX][MAX], int V, int s) {
-  
     // Create a queue for BFS
-    int q[MAX], front = 0, rear = 0;    
-
+    int q[MAX], front = 0, rear = 0;
 
     // Initially mark all the vertices as not visited
-    // When we push a vertex into the q, we mark it as 
-    // visited
-    bool visited[MAX] = { false };
-    
+    // When we push a vertex into the q, we mark it as visited
+    int visited[MAX] = { 0 };  // Use 0 for "false"
+
     // Mark the source node as visited and enqueue it
-    visited[s] = true;
+    visited[s] = 1;  // Use 1 for "true"
     q[rear++] = s;
-    
+
     // Iterate over the queue
     while (front < rear) {
-      
         // Dequeue a vertex and print it
         int curr = q[front++];
         printf("%d ", curr);
-        
+
         // Get all adjacent vertices of the dequeued vertex
         // If an adjacent has not been visited, mark it visited and enqueue it
         for (int i = 0; i < V; i++) {
             if (adj[curr][i] == 1 && !visited[i]) {
-                visited[i] = true;
+                visited[i] = 1;  // Mark as "true"
                 q[rear++] = i;
             }
         }
     }
 }
+
 
 
 // Function to add an edge to the graph
