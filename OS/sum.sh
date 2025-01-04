98573f4d73e2132c89c49a9dbb1df439b406bb2c
NIMA FATHIMA <nimafathimapr@gmail.com>
2025-01-04 11:48:37 +0530

    Update sum.sh

--- OS/sum.sh
+++ OS/sum.sh
@@ -8,3 +8,10 @@ sum=$(($sum + $l))
 n=$(($n / 10))
 done
 echo "SUM = $sum"
+
+#student@dl-15:~/NIMA_46$ gedit sum.sh
+#student@dl-15:~/NIMA_46$ chmod +x sum.sh
+#student@dl-15:~/NIMA_46$ ./sum.sh
+#ENTER NUMBER : 
+#865
+#SUM = 19
