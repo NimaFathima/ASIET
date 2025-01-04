4fd60d5130f527242f185105489b9993366bde55
NIMA FATHIMA <nimafathimapr@gmail.com>
2025-01-04 11:55:42 +0530

    Update palindrome.sh

--- OS/palindrome.sh
+++ OS/palindrome.sh
@@ -14,3 +14,15 @@ echo "THE NUMBER IS PALINDROME"
 else
 echo "THE NUMBER IS NOT PALINDROME"
 fi
+#student@dl-15:~/NIMA_46$ gedit palindrome.sh
+#student@dl-15:~/NIMA_46$ chmod +x palindrome.sh
+#student@dl-15:~/NIMA_46$ ./palindrome.sh
+#ENTER NUMBER : 
+#868
+#THE NUMBER IS PALINDROME
+#student@dl-15:~/NIMA_46$ ./palindrome.sh
+#ENTER NUMBER : 
+#856
+#THE NUMBER IS NOT PALINDROME
+#student@dl-15:~/NIMA_46$ 
+
