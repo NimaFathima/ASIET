ba88c3a261c131420c19703ac6e3ec0e1f8286a2
NIMA FATHIMA <nimafathimapr@gmail.com>
2025-01-04 12:04:11 +0530

    Update odd_even.sh

--- OS/odd_even.sh
+++ OS/odd_even.sh
@@ -6,3 +6,13 @@ echo "NUMBER IS EVEN"
 else 
 echo "NUMBER IS ODD"
 fi
+#student@dl-15:~/NIMA_46$ gedit oddeven.sh
+#student@dl-15:~/NIMA_46$ chmod +x oddeven.sh
+#student@dl-15:~/NIMA_46$ ./oddeven.sh
+#ENTER THE NUMBER: 
+#86
+#NUMBER IS EVEN
+#student@dl-15:~/NIMA_46$ ./oddeven.sh
+#ENTER THE NUMBER: 
+#85
+#NUMBER IS ODD
