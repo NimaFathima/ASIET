07d341654773a778efa6d2d24dddcf221eaf9b1c
NIMA FATHIMA <nimafathimapr@gmail.com>
2025-02-10 12:31:18 +0530

    Add files via upload

---
+++ OS/fcfs_new.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <string.h>
+
+void main() {
+    int n;
+
+    printf("Enter the number of processes: ");
+    scanf("%d", &n);
+    int pid[100],at[100],bt[100],ct[100],tt[100],wt[100];
+
+    int i;
+    float avg_wt=0,avg_tt=0;
+    for (i = 0; i < n; i++) {
+        printf("Enter the Process PID: ");
+        scanf("%d", &pid[i]);
+        printf("Enter the Process Burst time: ");
+        scanf("%d", &bt[i]);               
+    }
+    wt[0] = 0; 
+    for (i = 1; i < n; i++) {
+        wt[i]=wt[i-1]+bt[i-1];
+        avg_wt = avg_wt + wt[i];        
+    }
+    for (i = 0; i < n; i++) {
+        tt[i]=wt[i]+bt[i]; 
+        avg_tt = avg_tt + tt[i];        
+    }
+    
+    avg_wt= avg_wt/n; 
+    avg_tt= avg_tt/n; 
+    
+    printf("AVERAGE WAITING TIME: %f\n", avg_wt);
+    printf("AVERAGE TURN-AROUND TIME: %f\n", avg_tt);
+    
+    printf("GANTT CHART \n");
+    printf("______________________________________________________________ \n");
+    for (i = 0; i < n; i++) {
+    	printf("|	%d	",pid[i]);
+    	printf("|");	
+    }
+    printf("\n");
+    printf("______________________________________________________________ \n");
+    for (i = 0; i < n; i++) {
+    	printf("%d		",wt[i]);
+    }
+ }
+    	
+    
+
+
