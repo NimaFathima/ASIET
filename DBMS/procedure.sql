05dc115a63e4cddf5a1d20f37938d87f664ad6e1
NIMA FATHIMA <nimafathimapr@gmail.com>
2025-03-24 11:28:43 +0530

    Create procedure.sql

---
+++ DBMS/procedure.sql
@@ -0,0 +1,22 @@
+DECLARE
+	a number;
+	b number;
+	c number;
+
+PROCEDURE findMin(x IN number,y IN number,z OUT number) IS
+BEGIN
+	if x<y then
+		z:=x;
+	else
+		z:=y;
+		
+	END if;
+END;
+
+BEGIN
+	a:=23;
+	b:=45;
+	findMin(a,b,c);
+	dbms_output.put_line('Minimum of(23,45):'||c);
+END;
+/
