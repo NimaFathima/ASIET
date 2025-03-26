7dcfc11fc2ba09b9c4b60cb41252eefacb8505f8
NIMA FATHIMA <nimafathimapr@gmail.com>
2025-03-26 20:51:02 +0530

    Update sjf.c

--- OS/scheduling/sjf.c
+++ OS/scheduling/sjf.c
@@ -1,3 +1,5 @@
+SJF
+
 #include <stdio.h>
 
 void main() {
@@ -5,49 +7,44 @@ void main() {
     printf("Enter the number of processes: ");
     scanf("%d", &n);
 
-    int pid[100], at[100], bt[100], ct[100], tt[100], wt[100], rt[100];
-    int i, j, temp, time = 0, completed = 0;
+    int pid[100], at[100], bt[100], ct[100], tt[100], wt[100];
+    int is_completed[100] = {0}, execution_order[100], exec_count = 0;
+    int i, j, time = 0, completed = 0;
     float avg_wt = 0, avg_tt = 0;
 
-    // Input for processes, burst times
     for (i = 0; i < n; i++) {
         printf("Enter the Process PID: ");
         scanf("%d", &pid[i]);
         printf("Enter the Process Arrival Time: ");
         scanf("%d", &at[i]);
-        printf("Enter the Process Burst time: ");
+        printf("Enter the Process Burst Time: ");
         scanf("%d", &bt[i]);
-        rt[i] = bt[i]; // Remaining burst time
     }
 
-    // Main loop for Preemptive SJF Scheduling
+    // Process Execution
     while (completed < n) {
-        int selected_process = -1;
-        int shortest_burst_time = 10000;  // We are looking for the minimum burst time
+        int shortest = -1, min_bt = 9999;
 
-        // Find the process with the shortest remaining burst time that is ready to run
         for (i = 0; i < n; i++) {
-            if (rt[i] > 0 && at[i] <= time && rt[i] < shortest_burst_time) {
-                shortest_burst_time = rt[i];
-                selected_process = i;
+            if (at[i] <= time && is_completed[i] == 0 && bt[i] < min_bt) {
+                min_bt = bt[i];
+                shortest = i;
             }
         }
 
-        // If we found a valid process to run
-        if (selected_process != -1) {
-            rt[selected_process]--;  // Decrease the remaining burst time
-            time++;  // Increment the time
-
-            // If the process has finished its burst time
-            if (rt[selected_process] == 0) {
-                completed++;  // Mark the process as completed
-                ct[selected_process] = time;  // Completion time
-                tt[selected_process] = ct[selected_process] - at[selected_process];  // Turnaround time
-                wt[selected_process] = tt[selected_process] - bt[selected_process];  // Waiting time
-            }
-        } else {
-            time++;  // If no process is available, just increment time
+        if (shortest == -1) {
+            time++; // CPU idle time
+            continue;
         }
+
+        execution_order[exec_count++] = pid[shortest]; // Store execution order
+        time += bt[shortest];
+        ct[shortest] = time;
+        tt[shortest] = ct[shortest] - at[shortest];
+        wt[shortest] = tt[shortest] - bt[shortest];
+
+        is_completed[shortest] = 1;
+        completed++;
     }
 
     // Calculate averages
@@ -55,35 +52,46 @@ void main() {
         avg_wt += wt[i];
         avg_tt += tt[i];
     }
-
     avg_wt /= n;
     avg_tt /= n;
 
-    // Output results
+    // Process Table
     printf("\nPROCESS TABLE\n");
-    printf("------------------------------------------------------------\n");
+    printf("+-----+--------------+------------+----------------+-----------------+--------------+\n");
     printf("| PID | Arrival Time | Burst Time | Completion Time | Turnaround Time | Waiting Time |\n");
-    printf("------------------------------------------------------------\n");
+    printf("+-----+--------------+------------+----------------+-----------------+--------------+\n");
     for (i = 0; i < n; i++) {
-        printf("| %d   | %d            | %d          | %d              | %d               | %d            |\n",
-                pid[i], at[i], bt[i], ct[i], tt[i], wt[i]);
+        printf("| %3d | %12d | %10d | %14d | %15d | %12d |\n",
+               pid[i], at[i], bt[i], ct[i], tt[i], wt[i]);
     }
-    printf("------------------------------------------------------------\n");
+    printf("+-----+--------------+------------+----------------+-----------------+--------------+\n");
 
-    // Output the averages
     printf("\nAVERAGE WAITING TIME: %.2f\n", avg_wt);
     printf("AVERAGE TURNAROUND TIME: %.2f\n", avg_tt);
 
-    // Print Gantt Chart
+    // Gantt Chart
     printf("\nGANTT CHART\n");
-    printf("____________________________________________________________________________________ \n");
-    for (i = 0; i < n; i++) {
-        printf("|  %d  ", pid[i]);
+    printf("-------------------------------------------------------------------\n");
+    printf("|");
+
+    time = 0;
+    for (i = 0; i < exec_count; i++) {
+        printf(" P%d |", execution_order[i]);
     }
-    printf("|\n");
-    printf("____________________________________________________________________________________ \n");
-    for (i = 0; i < n; i++) {
-        printf("%d        ", at[i]);
+    
+    printf("\n-------------------------------------------------------------------\n");
+
+    // Printing time below Gantt Chart
+    printf("0");
+    time = 0;
+    for (i = 0; i < exec_count; i++) {
+        for (j = 0; j < n; j++) {
+            if (pid[j] == execution_order[i]) {
+                time += bt[j];
+                printf("   %d", time);
+                break;
+            }
+        }
     }
     printf("\n");
 }
