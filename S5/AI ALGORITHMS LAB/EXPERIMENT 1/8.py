5cdef17553b344c4c2de6e06e9e0f2548ec286a1
NIMA FATHIMA <nimafathimapr@gmail.com>
2025-07-07 11:16:39 +0530

    Update 8.py

--- S5/AI ALGORITHMS LAB/EXPERIMENT 1/8.py
+++ S5/AI ALGORITHMS LAB/EXPERIMENT 1/8.py
@@ -1,8 +1,8 @@
 while True: 
-       c=int(input(" 1.ADDITION \n2.SUBTRACTION \n3.MULTIPLICATION \n4.DIVISION \n5.Exit \nEnter your choice:  "))
        a=eval(input("Enter the first number: "))
        b=eval(input("Enter the second number: "))
-
+       c=int(input(" 1.ADDITION \n2.SUBTRACTION \n3.MULTIPLICATION \n4.DIVISION \n5.Exit \nEnter your choice:  "))
+       
        if c==1:
               print("RESULT AFTER ADDITION: ",a+b)
        elif c==2:
